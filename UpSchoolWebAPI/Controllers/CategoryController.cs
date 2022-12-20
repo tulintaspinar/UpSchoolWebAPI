9f4001ea1a6083fe6d64231766681031bab43e40
Tülin Taşpınar <tulinttaspinar@gmail.com>
2022-12-20 13:09:21 +0300

    Add project files.

---
+++ UpSchoolWebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UpSchoolWebAPI.DAL;
+using UpSchoolWebAPI.DAL.Entities;
+
+namespace UpSchoolWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		Context context = new Context();
+		[HttpGet]
+		public IActionResult CategoryList()
+		{
+			var values = context.Categories.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CategoryAdd(Category category)
+		{
+			var value = context.Categories.Add(category);
+			context.SaveChanges();
+			return Ok();
+		}
+		[HttpPut]
+		public IActionResult CategoryUpdate(Category category)
+		{
+			var value = context.Categories.Update(category);
+			context.SaveChanges();
+			return Ok();
+		}
+		[HttpDelete("{id}")]
+		public IActionResult CategoryDelete(int id)
+		{
+			var category = context.Categories.Find(id);
+			context.Categories.Remove(category);
+			context.SaveChanges();
+			return Ok();
+		}
+	}
+}
---
+++ UpSchoolWebAPI/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UpSchoolWebAPI.DAL.Entities;
+
+namespace UpSchoolWebAPI.DAL
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=.;initial catalog=UpSchoolAPI;integrated security=true;");
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
